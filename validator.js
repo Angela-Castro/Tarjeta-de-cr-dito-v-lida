83b153a7208f59bfb042517aa65089b636c7abe4
Angela-Castro <65985094+Angela-Castro@users.noreply.github.com>
2020-06-16 11:41:54 -0500

    Update validator.js

--- validator.js
+++ validator.js
@@ -1,5 +1,18 @@
 const validator = {
-  // ...
+// Algoritmo de Luhn
+  function (creditCardNumber){
+  var sum = 0;
+  var numdigits = creditCardNumber.length;
+  var parity = numdigits % 2;
+  for(var i=0; i < numdigits; i++) {
+    var digit = parseInt(creditCardNumber.charAt(i))
+    if(i % 2 == parity) digit *= 2;
+    if(digit > 9) digit -= 9;
+    sum += digit;
+  }
+  return (sum % 10) == 0;
+}
+};
 };
 
 export default validator;
