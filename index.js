84fd264406665784cc94b4728dab6cad6f8fe20b
Angela-Castro <65985094+Angela-Castro@users.noreply.github.com>
2020-06-16 11:40:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import validator from './validator.js';
-// Algoritmo de Luhn
-  function (creditCardNumber){
-  var sum = 0;
-  var numdigits = creditCardNumber.length;
-  var parity = numdigits % 2;
-  for(var i=0; i < numdigits; i++) {
-    var digit = parseInt(creditCardNumber.charAt(i))
-    if(i % 2 == parity) digit *= 2;
-    if(digit > 9) digit -= 9;
-    sum += digit;
-  }
-  return (sum % 10) == 0;
-}
-};
+// Llamamos a los atributos
+const tarjeta = document.getElementById("nombre");
+const numeroTarjeta = document.getElementById("creditCardNumber");
+const boton = document.getElementById("caja-btn");
+// Variables de validación 
+const number = /^([0-9])*$/;
+const validateVisa = /^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/;
+const validateMastercard = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;
+const validateAmericanExpress = /^3[47][0-9]{13}$/;
+const validateDiscover = /^6(?:011|5[0-9]{2})[0-9]{12}$/;
+//Función que valida que sólo ingresen números en la tarjeta
+const creditCardNumber = (num) => {
+	if (number.test(num)) {
+	  return true;
+	}
+  };
 console.log(validator);
